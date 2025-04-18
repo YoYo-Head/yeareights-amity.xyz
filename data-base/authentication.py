11151a980d3767c0ca079f50888ee12db28fd591
YoYo-Head <yoyohead438@gmail.com>
2025-04-18 15:37:46 +1000

    new file:   data-base/authentication.py 	new file:   data-base/denied/index.html 	new file:   data-base/index.html 	new file:   data-base/info/incomeData.py 	new file:   data-base/info/index.html 	new file:   data-base/info/loginData.json 	modified:   homepage/index.html 	new file:   links/index.html 	new file:   links/main.css 	modified:   server-list/index.html

---
+++ data-base/authentication.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import socket as clct
+import time
+
+app = Flask(__name__)
+
+checker = True
+myIP = '203.221.138.117'
+
+t = time.localtime()
+
+# How often should the program check for IP (put 62 for only once every minute )
+oftenCheck = 5
+
+@app.route('/')
+def authenticate():
+
+    render_template('index.html')
+
+    secs = str(t.tm_sec)
+    checkTime = (int(secs) % oftenCheck) -1
+
+    hostName = clct.gethostname()
+    IPadrs = clct.gethostbyname(hostName)
+
+    if checkTime == 0 or secs == 1:
+        if IPadrs == myIP:
+            print("redirecting to data base")
+
+        else:
+            print("You are not authorised to access!")
+            return render_template('denied/')
+
+if __name__ == '__main__':
+    app.run(debug=False)
