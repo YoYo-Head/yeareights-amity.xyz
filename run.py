7dd5f5ed01f105d3f00e34307a77dbde28482686
YoYo-Head <yoyohead438@gmail.com>
2025-06-13 18:31:40 +1000

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import app
 import os
 
-port = int(os.environ.get('PORT', 7000))
+port = int(os.environ.get('PORT', 10000))
 
 if __name__ == "__main__":
     app.run(debug=False, port=port) 
